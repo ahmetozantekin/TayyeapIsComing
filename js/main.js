cd55bc527433bd540665307d811fddd54a206ba1
Mahmut Akyol <mahmut.akyol.90@gmail.com>
2015-04-15 01:14:15 +0300

    -

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 /**
  * Created by MacBookAir on 13/04/15.
  */
+
 $(document).keydown(function(e){
+    var maxWidth = $('.main').width();
+    var maxHeight = $('.main').height();
     var key = e.keyCode ? e.keyCode : e.which;
 
     switch (key){
         case 37:
+            if($('.tyyp').css('margin-left') < 0){
+                $('.tyyp').animate({ 'marginLeft': '0px' });
+            }else
             $('.tyyp').animate({ 'marginLeft': '-=50px' }, 0);
             break;
         case 38:
             $('.tyyp').animate({ 'marginTop': '-=50px' }, 0);
             break;
         case 39:
+            if($('.tyyp').css('margin-left') == maxWidth){
+                $('.tyyp').animate({ 'marginLeft': '0px' });
+            }
             $('.tyyp').animate({ 'marginLeft': '+=50px' }, 0);
             break;
         case 40:
@@ -19,3 +28,4 @@ $(document).keydown(function(e){
             break;
     }
 });
+
